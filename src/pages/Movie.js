4916c8952af93d67aa1572d294d626e132188fef
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-08-09 17:33:09 +0300

    cannot diplay the movies yrt but will be fixed soon

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import React from "react";
 
 function Movie() {
+  const [movies, setMovies] = useState({});
+
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() =>{
+      fetch(`http://localhost:4000/movies/${movieId}`)
+      .then(r => r.json())
+      .then(data => setMovies(data))
+      .catch(error => console.error(error));
+    }, [movieId]);
+
+    const gen =movies.genres.map((genre ,index)=>(<span key = {index}>{genre}</span>
+    ))
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movies.title}</h1>
+        <p>{movies.time}</p>
+        {gen}
       </main>
     </>
   );
